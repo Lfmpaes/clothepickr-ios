a8a3b1b94917ae931aab5c0d87233289916521bb
Luiz Fernando Paes <luiz@paes.dev>
2023-09-01 09:26:56 -0300

    Initial Commit

---
+++ ClothePickr/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ClothePickr
+//
+//  Created by Luiz Fernando Paes on 01/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
